1d7f691a214e87c6e5bab531d70805e114f08013
bernardocerqueira2612 <159343790+bernardocerqueira2612@users.noreply.github.com>
2024-03-27 14:58:51 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,50 @@
+def soma (a, b):
+    return a + b
+
+def quadrado (a):
+    return a**2
+
+def hipotenusa (cateto1, cateto2):
+    return soma(quadrado(cateto1), quadrado(cateto2))**(1/2)
+
+def simples(nome):
+            if nome == 'bernardo':
+                return 'Acertou meu nome'
+
+def medio(nome):
+    if nome == 'bernardo':
+        return 'ACERTOU meu nome'
+    else :
+        return 'voce nao conhece meu nome'
+
+
+def completo(nome):
+    if nome == 'bernardo':
+        return 'Acertou meu nome'
+    elif nome == 'sei la':
+        return 'vai pro karalho'
+    else:
+        return 'Voce nao sabe meu nome'
+
+
+numeros = [1,2,3,4,5]
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+for i in range(10):
+    print(i)
+
+for item in [1,45,78,'a',[3,5]]:
+    print(item)
+
+for letra in 'minha string':
+    print(letra)
+
+
+for i in range(4):
+    print((i + 1) * "*")
+    
+            
